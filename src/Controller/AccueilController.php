e972a7e099ac9499aa6234da17f20bd39b7cf0b3
Dylan Groux <dylangroux2105@gmail.com>
2025-07-09 21:58:24 +0200

    Ajout du system d'envois de mail dans le formulaire

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 use App\Form\ContactType;
 use App\Repository\FormationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 /**
  * Description of AccueilController
@@ -15,22 +18,46 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class AccueilController extends AbstractController{
 
+
+    public function redirectToAccueil(): Response{
+        $url = $this->generateUrl('accueil') . '#contact';
+        return new RedirectResponse($url);
+    }
     
     #[Route('/', name: 'accueil')]
-    public function index(Request $request): Response{
+    public function index(Request $request, MailerInterface $mailer): Response{
 
         $form = $this->createForm(ContactType::class);
     
         $form->handleRequest($request);
     
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            // TODO: Ajoutez la logique d'envoi d'email ici
-            // $this->addFlash('success', 'Formulaire de contact rempli avec succès !');
 
-            $this->addFlash('info', 'En cours de développement, veuillez réessayer plus tard, ou me contacter à l\'adresse mail dylangroux2105@gmail.com');
-    
-            return $this->redirectToRoute('accueil');
+            // Construction du mail
+            $email = (new Email())
+                ->from($data['email']) // l'expéditeur : le champ du formulaire
+                ->to('admin@portfolio.grdy2507.odns.fr') // le destinataire final
+                ->subject($data['subject'])
+                ->text(
+                    "Nom : " . $data['name'] . "\n" .
+                    "Email : " . $data['email'] . "\n" .
+                    "Téléphone : " . $data['phone'] . "\n\n" .
+                    "Message : \n" . $data['message']
+                );
+
+            // Envoi du mail
+            $mailer->send($email);
+
+            // Ajout d’un message flash de confirmation
+            $this->addFlash('info', 'Votre message a bien été envoyé. Merci !');
+
+            // Redirection pour éviter le re-post en cas de rafraîchissement
+            $url = $this->generateUrl('accueil') . '#contact';
+            return new RedirectResponse($url);
         }
 
         return $this->render("pages/accueil.html.twig", [
